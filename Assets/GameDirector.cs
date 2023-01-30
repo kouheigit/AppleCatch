948fecbab6d911f46abce3c164a4754f8a92b1a2
kouheigit <ookawakouhei777@gmail.com>
2023-01-31 00:13:52 +0900

    dev 5

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -26,11 +26,32 @@ public class GameDirector : MonoBehaviour
     {
         this.timerText = GameObject.Find("Time");
         this.pointText = GameObject.Find("Point");
+        this.generator = GameObject.Find("ItemGenerator");
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.time = 0;
+        if(this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f, 0, 0);
+        }else if(0 <= this.time && this.time < 5){
+
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 3);
+
+        }else if(5 <= this.time && this.time < 10){
+
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -0.05f, 6);
+
+        }else if(10 <= this.time && this.time < 20){
+
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -0.04f, 4);
+
+        }else if(20 <=this.time && this.time < 30){
+            this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -0.03f, 2);
+        }
         this.time -= Time.deltaTime;
         this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
         this.pointText.GetComponent<TextMeshProUGUI>().text = this.point.ToString() + " point";
